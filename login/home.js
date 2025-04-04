7d8329d2a16b9337afc42d257bac1567302057e0
tomhoon <gnsdl9108@naver.com>
2025-04-04 18:20:08 +0900

    feat: 로그인, 회원가입 추가

---
+++ login/home.js
@@ -0,0 +1,88 @@
+const clickParticle = (e) => {
+  for (let i = 0; i < 10; i++) {
+    const particle = document.createElement('div');
+    particle.className = 'particle';
+
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const angle = Math.random() * 2 * Math.PI;
+    const radius = Math.random() * 60;
+
+    particle.style.left = `${x}px`;
+    particle.style.top = `${y}px`;
+    particle.style.setProperty('--x', `${Math.cos(angle) * radius}px`);
+    particle.style.setProperty('--y', `${Math.sin(angle) * radius}px`);
+
+    document.body.appendChild(particle);
+
+    setTimeout(() => particle.remove(), 600);
+  }
+}
+document.addEventListener('click', clickParticle);
+
+const getAllUserInfo = () => {
+  const userId = document.querySelector('#id').value;
+  const pw = document.querySelector('#password').value;
+
+  if (!userId || !pw) return 0;
+
+  return {
+    userId,
+    pw
+  }
+}
+
+const goLogin = async () => {
+  const url = 'http://tomhoon.duckdns.org:18085/login';
+  const param = getAllUserInfo();
+
+  if (!param) {
+    alert("아이디 비밀번호 입력");
+    return;
+  }
+
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(param)
+  });
+
+  const data = await res.json();
+
+  if (data?.resultCode == 100) {
+    alert('로그인 성공');
+  } else {
+    alert(data?.result || '로그인 실패');
+  }
+
+}
+
+const JOINBTN = document.querySelector('.joinbtn');
+const PLAYBTN = document.querySelector('.playbutton');
+const password = document.querySelector('#password');
+const id = document.querySelector('#id');
+
+PLAYBTN.addEventListener('click', (e) => {
+  goLogin();
+});
+
+JOINBTN.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  location.href = './join.html';
+
+});
+
+password.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
+    goLogin();
+  }
+})
+id.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
+    goLogin();
+  }
+})
---
+++ login/join.js
@@ -0,0 +1,44 @@
+const getAllUserInfo = () => {
+  const userId = document.querySelector('#id').value;
+  const pw = document.querySelector('#password').value;
+  const nickname = document.querySelector('#nickname').value;
+
+  if (!userId || !pw || !nickname) return 0;
+
+  return {
+    userId,
+    pw,
+    nickname
+  }
+}
+
+const submit = async () => {
+  const url = 'http://tomhoon.duckdns.org:18085/join';
+  const param = getAllUserInfo();
+
+  if (!param) {
+    alert('모두 채워주세요');
+    return;
+  }
+
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(param)
+  });
+
+  const data = await res.json();
+
+  if (data?.resultCode == 100) {
+    alert(data?.result);
+  } else {
+    alert(data?.result || '회원가입 실패');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const SUBMITBTN = document.querySelector('.submitbtn');
+  SUBMITBTN.addEventListener('click', submit);
+});
